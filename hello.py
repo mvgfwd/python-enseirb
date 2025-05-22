98e6e7d6fa5949acc9516ca6a8b13fa3b8d36c48
mvgfwd <jonajr05@gmail.com>
2025-05-22 14:55:53 +0200

    [update]hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
-print("Hello World")
+#!/usr/bin/env python3
 
+#print("Hello World")
+
+def say_hello(whom='world'):
+    print(f"Hello {whom}")
+
+
+#Magical Receipt
+if __name__ == '__main__':
+    import sys
+
+    if len(sys.argv) > 1 :
+        say_hello(sys.argv[1])
+        
+    say_hello()
