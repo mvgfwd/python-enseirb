69e0efaaa37b504ac219ad30280edd7ecdca7e79
mvgfwd <yonathan.s@gmf-aeroasia.co.id>
2025-06-08 23:27:04 +0700

    [update]pokemon add cache

--- pokemon_project/pokemon/__init__.py
+++ pokemon_project/pokemon/__init__.py
@@ -2,6 +2,8 @@
 import sys
 import requests
 import json
+import platformdirs
+import os.path
 
 """
 Pokemon exercise
@@ -9,6 +11,8 @@ Pokemon exercise
 
 __version__ = "0.0.1"
 
+
+CACHE_DIR = platformdirs.user_cache_dir('pokemon')
 
 class Pokemon:
     def __init__(self, id, name, height, weight, types, stats, moves, sprite_url, **kwargs):
@@ -28,16 +32,21 @@ class Pokemon:
         legend_api = requests.get(f"{LEGENDARY_API_URL}{id_or_name}")
         legend_data = legend_api.content
         legend_load = json.loads(legend_data)
-
         is_legendary = legend_load['is_legendary']
         new_poke = LegendaryPokemon(**data) if is_legendary else Pokemon(**data)
         return new_poke
 
     def fetch(self, pokemon_name):
-        POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
-        poke_api = requests.get(f"{POKE_API_URL}{pokemon_name}")
-        poke_data = poke_api.content
-        data = json.loads(poke_data)
+        if self.retrieve_cache(pokemon_name):
+            data = self.retrieve_cache(pokemon_name)
+            print("FROM CACHE")
+        else:
+            POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+            poke_api = requests.get(f"{POKE_API_URL}{pokemon_name}")
+            poke_data = poke_api.content
+            data = json.loads(poke_data)
+            self.cache_res(data, pokemon_name)
+            print("NEW CACHE")
         #the data
         res = {
             "id" : data['id'],
@@ -45,19 +54,30 @@ class Pokemon:
             "height" : data['height'] ,
             "weight" : data['weight'] ,
             "types" : [[x['type']['name'] for x in data['types']]][0],
-            "stats" : {x['stat']['name']: x['base_stat'] for x in data['stats']}, #FIXME
-            "moves" : [x['move']['name'] for x in data['moves']], #FIXME
+            "stats" : {x['stat']['name']: x['base_stat'] for x in data['stats']},
+            "moves" : [x['move']['name'] for x in data['moves']],
             "sprite_url" : data['sprites']['front_default']
         }
         return res
+    
+    def cache_res(json_data, po_name):
+        os.path.join(platformdirs.user_cache_dir('pokemon'))
+        os.makedirs(CACHE_DIR, exist_ok=True)
+        f = open(f'{CACHE_DIR}/{po_name.lower()}.json', 'w')
+        f.write(json.dumps(json_data))
+        f.close()
+
+    def retrieve_cache(poke):
+        try: #take from cache
+            with open(f'{CACHE_DIR}/{poke.lower()}.json', 'r') as f:
+                return json.load(f)
+        except: #create the cache
+            return False
 
     def print_info(self):
         print(f"Ordinary Poke: {self.name.capitalize()}")
         for k, v in self.stats.items():
             print(f"{k}: {v}")
-
-    def assign_to_legendary():
-        pass
 
 
 class LegendaryPokemon(Pokemon):
